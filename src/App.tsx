b765e6ee20e49c97609c9ced09e99906ce66b919
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2024-03-14 00:01:55 +0100

    basic structure + calendar component with basic functional

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './index.css'
+import {router} from "./router/Router.tsx";
+import {RouterProvider} from "react-router-dom";
+function App() {
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import MonthlyCalendar from "../components/MonthlyCalendar.tsx";
+
+export const Home = () => {
+  return (
+    <div className='max-w-[1200px] mx-auto h-screen p-[15px]'>
+      <MonthlyCalendar />
+    </div>
+  );
+}
